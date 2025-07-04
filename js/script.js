e06e77623ff8b99d03912d52e420d2f85c032ebd
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-04 16:19:56 +0200

    [add] fix variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Mail
 let invitato;
+let mailInvitati = ['pippo@gmail.com', 'claudio@icloud.com', 'piero@libero.it', 'pluto@gmail.com'];
 const numInvitati = parseInt(prompt('Quanti invitati ci saranno?'));
 for (i = 0; i < numInvitati; i++) {
 
