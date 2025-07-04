44a6d1fb889b7948592e4b33316cbffbff269bb9
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-04 16:43:46 +0200

    [add] feat comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 //Mail
 let invitato;
+//creo array con mail invitati
 let mailInvitati = ['pippo@gmail.com', 'claudio@icloud.com', 'piero@libero.it', 'pluto@gmail.com'];
 let mailOspite = prompt('Scrivi la tua mail');
+//ciclo for per controllare ogni posizione dell'array se contiene la mail inserita
 for (i = 0; i < mailInvitati.length; i++) {
   if (mailInvitati[i] === mailOspite) {
+    //se c'è una corrispondenza della mail, allora esco dal ciclo con il break
     invitato = "Sei invitato";
     break;
   }
